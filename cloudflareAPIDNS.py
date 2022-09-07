292d1dc77d9b8c383391796300c57b63959f164e
Jacob <54694270+BananasRule@users.noreply.github.com>
2022-09-07 16:24:24 +0800

    added license notices

--- cloudflareAPIDNS.py
+++ cloudflareAPIDNS.py
@@ -1,3 +1,9 @@
+##© Jacob Gray 2022 
+##This Source Code Form is subject to the terms of the Mozilla Public
+##License, v. 2.0. If a copy of the MPL was not distributed with this
+##file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
 import requests
 #Requests
 #Copyright 2019 Kenneth Reitz
--- mailSMTP.py
+++ mailSMTP.py
@@ -1,5 +1,12 @@
-import smtplib
+##© Jacob Gray 2022
+##This Source Code Form is subject to the terms of the Mozilla Public
+##License, v. 2.0. If a copy of the MPL was not distributed with this
+##file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 ## A class designed to send mail via STMP simplifed from stmplib
+
+import smtplib
+
 class mailSMTP:
     ##Initalise class
     # @param key API Key / Username (String)
